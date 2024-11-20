83c487eda7af3fe1aa87975c45cc03f6a6225215
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-11-20 15:24:48 +0300

    Work of developer_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello JavaDeveloper!");
+        System.out.println("This is branch of Developer-2");
     }
 }
