68f8efad6b2badef80b0c1dc453ef7a837d6f9b0
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-11-20 15:07:20 +0300

    Init commit

---
+++ src/main/java/org/example/ArrayListExample.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<Integer>numberList=List.of(9,2,3);
+        System.out.println(numberList.get(0));
+    }
+}
