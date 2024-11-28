19d19a1ab50e44a4cd81600a5558d8487e091a32
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-11-28 15:42:03 +0300

    Modification  of developer_2

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello JavaDeveloper!");
-        System.out.println("This is branch of Developer-2");
-    }
-}
